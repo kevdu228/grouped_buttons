379087a4dee8af849f1167c73364e03db7538d08
kevdu228 <52705453+kevdu228@users.noreply.github.com>
2023-12-29 18:20:59 -0500

    Update radio_button_group.dart

--- lib/src/radio_button_group.dart
+++ lib/src/radio_button_group.dart
@@ -51,7 +51,7 @@ class RadioButtonGroup extends StatefulWidget {
   final EdgeInsetsGeometry margin;
 
   RadioButtonGroup({
-    Key? key,
+    
     @required this.labels,
     this.picked,
     this.disabled,
@@ -63,7 +63,7 @@ class RadioButtonGroup extends StatefulWidget {
     this.itemBuilder,
     this.padding = const EdgeInsets.all(0.0),
     this.margin = const EdgeInsets.all(0.0),
-  }) : super (key?: key);
+  });
 
   @override
   _RadioButtonGroupState createState() => _RadioButtonGroupState();
